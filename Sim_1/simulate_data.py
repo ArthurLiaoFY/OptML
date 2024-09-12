95320b23decf836b6e55c44d954955f45ee0d36f
arthur <wr80340@smail.nchu.edu.tw>
2024-09-12 23:19:52 +0800

    #modify simulate data

--- Sim_1/simulate_data.py
+++ Sim_1/simulate_data.py
@@ -1,31 +1,38 @@
-from configparser import ConfigParser
-
-import pandas as pd
-from simulate_funcs import f, np
+import numpy as np
 from sklearn.model_selection import train_test_split
 
-config = ConfigParser()
-config.read("config.ini")
 
+class SimulateData:
+    def __init__(
+        self, seed: int = 1122, simulate_size: int = 3000, variation_ratio: float = 0.01
+    ) -> None:
+        self.seed = seed
+        self.simulate_size = simulate_size
+        self.variation_ratio = variation_ratio
+        self.xy_min = [-2.0, -2.0]
+        self.xy_max = [2.0, 2.0]
 
-seed = np.random.RandomState(int(config["simulate"].get("seed")))
+    def __f(self, x1, x2):
+        return (-np.cos(np.pi * (x1)) * np.cos(2 * np.pi * (x2))) / (
+            1 + np.power(x1, 2) + np.power(x2, 2)
+        )
 
-
-xy_min = [-2.0, -2.0]
-xy_max = [2.0, 2.0]
-
-model_matrix = seed.uniform(
-    low=xy_min,
-    high=xy_max,
-    size=(int(config["simulate"].get("simulate_size")), len(xy_max)),
-)
-y = np.array(
-    [
-        f(x[0], x[1]) + seed.randn() * float(config["simulate"].get("variation_ratio"))
-        for x in model_matrix
-    ]
-)
-
-train_X, val_X, train_y, val_y = train_test_split(
-    model_matrix, y, train_size=0.8, random_state=1122
-)
+    def get_data(self, train_size_ratio: float = 0.8):
+        seed = np.random.RandomState(self.seed)
+        model_matrix = seed.uniform(
+            low=self.xy_min,
+            high=self.xy_max,
+            size=(self.simulate_size, len(self.xy_max)),
+        )
+        y = np.array(
+            [
+                self.__f(x[0], x[1]) + seed.randn() * self.variation_ratio
+                for x in model_matrix
+            ]
+        )
+        return train_test_split(
+            model_matrix,
+            y,
+            train_size=train_size_ratio,
+            random_state=self.seed,
+        )  # train_X, val_X, train_y, val_y
--- Sim_1/simulate_funcs.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-
-def f(x1, x2):
-    return (-np.cos(np.pi * (x1)) * np.cos(2 * np.pi * (x2))) / (
-        1 + np.power(x1, 2) + np.power(x2, 2)
-    )
